c6bbd60b55d7b2a16212fb3212af5d6f83114bac
NickSwag <1282622481@qq.com>
2018-11-21 18:46:26 +0800

    20181121-orm初步

---
+++ www/orm.py
@@ -0,0 +1,75 @@
+__Author__ = "noduez"
+
+import asyncio, logging
+import aiomysql
+
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
+async def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global  __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
+# select
+async def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    with(await __pool) as conn:
+        cur = await conn.cursor(aiomysql.DictCursor)
+        await cur.execute(sql.replace('?', '%s'), args or ())
+        if size:
+            rs = await cur.fetchmany(size)
+        else:
+            rs = await cur.fetchall()
+        await cur.close()
+        logging.info('rows return: %s' % len(rs))
+        return rs
+
+# insert, update, delete
+async def execute(sql, args, autocommit=True):
+    log(sql)
+    with (await __pool) as conn:
+        try:
+            cur = await conn.cursor()
+            await cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            await cur.close()
+        except BaseException as e:
+            raise
+        return affected
+
+
+
+## 定义Model基类
+class Model(dict):#, metaclass=ModelMetaclass)
+
+    def __init__(self):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, item):
+        try:
+            return self[item]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s" % item)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
