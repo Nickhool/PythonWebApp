28527e58bcca537cc891767a1c1c4b9336a9122a
NickSwag <1282622481@qq.com>
2018-12-07 09:04:32 +0800

    Day05

--- www/dbtest.py
+++ www/dbtest.py
@@ -1,1 +1,19 @@
 __Author__ = "noduez"
+
+import asyncio
+import www.orm
+from www.models import User, Blog, Comment
+
+async def test(loop):
+    await www.orm.create_pool(loop, user='root', password='123456', database='awesome')
+
+    u = User(name='adminz_sj712', admin=True, email='1282622481@qq.com', passwd='awesome12593', image='about:blank')
+
+    await u.save()
+
+#要运行协程，需要使用事件循环
+if __name__ == '__main__':
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(test(loop))
+        print('Test finished.')
+        loop.close()
--- www/models.py
+++ www/models.py
@@ -20,7 +20,7 @@ class User(Model):
     admin = BooleanField()
     name = StringField(ddl='varchar(50)')
     image = StringField(ddl='varchar(500)')
-    create_at = FloatField(default=time.time)
+    created_at = FloatField(default=time.time)
 
 class Blog(Model):
     __table__ = 'blogs'
@@ -32,7 +32,7 @@ class Blog(Model):
     name = StringField(ddl='varchar(50)')
     sunmmary = StringField(ddl='varchar(200)')
     content = TextField()
-    create_at = FloatField(default=time.time)
+    created_at = FloatField(default=time.time)
 
 class Comment(Model):
     __table__ = 'comments'
@@ -43,4 +43,4 @@ class Comment(Model):
     user_name = StringField(ddl='varchar(50)')
     user_image = StringField(ddl='varchar(500)')
     content = TextField()
-    create_at = FloatField(default=time.time)
+    created_at = FloatField(default=time.time)
